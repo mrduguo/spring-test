8bec7919d9befc7a191b2b22120ce57f7265e12c
Guo Du <mrduguo@gmail.com>
2018-03-18 00:30:50 +0000

    init checkin

---
+++ src/main/groovy/com/github/mrduguo/spring/test/config/HealthSpec.groovy
@@ -0,0 +1,30 @@
+package com.github.mrduguo.spring.test.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
+
+class HealthSpec extends AbstractSpec {
+
+    @Value('${server.contextPath:}')
+    String contextPath
+
+    void "web app should be health"() {
+        when:
+        def entity = getForEntity(contextPath+'/health', Map.class)
+
+        then:
+        entity.statusCode == HttpStatus.OK
+        entity.body.status == 'UP'
+        entity.body.diskSpace.status == 'UP'
+
+        keepServiceRunningWhenRunTheTestIndividually()
+    }
+
+    void keepServiceRunningWhenRunTheTestIndividually() {
+        if (System.properties.getProperty('sun.java.command')?.endsWith('HealthSpec') || System.properties.getProperty('test.single')?.endsWith('HealthSpec')) {
+            println "\nRun test in service mode, you may access the swagger api spec at:\nhttp://localhost:${port}${contextPath}/swagger-ui.html\n"
+            Thread.sleep(Long.MAX_VALUE)
+        }
+    }
+
+}
---
+++ src/main/groovy/com/github/mrduguo/spring/test/config/SwaggerApiValidationSpec.groovy
@@ -0,0 +1,59 @@
+package com.github.mrduguo.spring.test.config
+
+import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
+
+class SwaggerApiValidationSpec extends AbstractSpec {
+
+    @Value('${server.contextPath:}')
+    String contextPath
+
+    @Value('${server.host:localhost:8080}')
+    String serverHost
+
+    void "generated spec should match swagger.json"() {
+        given:
+        System.properties.setProperty('jdk.map.althashing.threshold','512') // keep json map order
+        def swaggerJson = new JsonSlurper().parseText(getClass().getResourceAsStream('/static/swagger.json').text)
+        def swaggerSpec=JsonOutput.prettyPrint(new JsonBuilder(swaggerJson).toString())
+        def targetFile=new File('build/resources/test/generated/swagger.json')
+        targetFile.delete()
+        targetFile.parentFile.mkdirs()
+
+        when:
+        def entity = getForEntity(contextPath+'/v2/api-docs', String.class)
+
+        then:
+        entity.statusCode == HttpStatus.OK
+        def apiJson=new JsonSlurper().parseText(entity.body)
+        apiJson.put('host', serverHost)
+        def generatedSpec=JsonOutput.prettyPrint(new JsonBuilder(apiJson).toString())
+        targetFile.write(generatedSpec)
+        generatedSpec==swaggerSpec
+    }
+
+    void "generated dependencies api should match swagger-mock.json"() {
+        given:
+        System.properties.setProperty('jdk.map.althashing.threshold','512') // keep json map order
+        def swaggerJson = new JsonSlurper().parseText(getClass().getResourceAsStream('/static/swagger-dependencies.json').text)
+        def swaggerSpec=JsonOutput.prettyPrint(new JsonBuilder(swaggerJson).toString())
+        def targetFile=new File('build/resources/test/generated/swagger-dependencies.json')
+        targetFile.delete()
+        targetFile.parentFile.mkdirs()
+
+        when:
+        def entity = getForEntity(contextPath+'/v2/api-docs?group=dependencies', String.class)
+
+        then:
+        entity.statusCode == HttpStatus.OK
+        def apiJson=new JsonSlurper().parseText(entity.body)
+        apiJson.put('host', serverHost)
+        def generatedSpec=JsonOutput.prettyPrint(new JsonBuilder(apiJson).toString())
+        targetFile.write(generatedSpec)
+        generatedSpec==swaggerSpec
+    }
+
+}
