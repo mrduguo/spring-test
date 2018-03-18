9ff81652a7590ef3bbe02c322cb870116851bd46
Guo Du <mrduguo@gmail.com>
2018-03-18 23:34:43 +0000

    updated swagger support

---
+++ src/test/groovy/com/github/mrduguo/spring/test/HealthSpec.groovy
@@ -0,0 +1,5 @@
+package com.github.mrduguo.spring.test
+
+class HealthSpec extends com.github.mrduguo.spring.test.config.HealthSpec {
+
+}
---
+++ src/test/groovy/com/github/mrduguo/spring/test/SwaggerApiValidationSpec.groovy
@@ -0,0 +1,5 @@
+package com.github.mrduguo.spring.test
+
+class SwaggerApiValidationSpec extends com.github.mrduguo.spring.test.config.SwaggerApiValidationSpec {
+
+}
